a049691780c4dab1f16a3a799e783a3159a93b01
pa1zore <pavanzore345@gmail.com>
2023-12-06 21:25:50 +0530

    Modified: Check employee is present or absent

--- src/com/bridgelabz/Employee_Wage_Computation.java
+++ src/com/bridgelabz/Employee_Wage_Computation.java
@@ -1,7 +1,20 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class Employee_Wage_Computation {
+    final static int check=1;
     public static void main(String[] args) {
         System.out.println("Welcom to Employee Wage Computation");
+        Random r= new Random();
+        int current=r.nextInt(2);
+        if(check==current)
+        {
+            System.out.println("employee is present");
+        }
+        else
+        {
+            System.out.println("employee is absent");
+        }
     }
 }
