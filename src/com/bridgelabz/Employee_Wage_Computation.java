27f5cb6d56ad869b160d027e0eb3146c04d15e1a
pa1zore <pavanzore345@gmail.com>
2023-12-06 21:38:27 +0530

    Modified: Calculate till total working hour or days is reached for a month

--- src/com/bridgelabz/Employee_Wage_Computation.java
+++ src/com/bridgelabz/Employee_Wage_Computation.java
@@ -7,14 +7,17 @@ public class Employee_Wage_Computation {
     final static int part_time=2;
     final static int Num_of_working_day=20;
     final static int Daily_wage_per_hour=20;
+    final static int Max_hrs_in_month=100;
     public static void main(String[] args) {
         //variables
         int empHr=0;
-        int empWwage=0;
-        int totalEmpWage=0;
+        //int empWwage=0;
+        int totalEmpHrs=0;
+        int totalWorkingDays=0;
         // 20 iterations for working days in month
-        for(int day = 0;day<Num_of_working_day;day++)
+        while(totalEmpHrs<=Max_hrs_in_month && totalWorkingDays< Num_of_working_day)
         {
+            totalWorkingDays++;
             Random r= new Random();
             int current=r.nextInt(3);
             switch (current) {
@@ -32,11 +35,12 @@ public class Employee_Wage_Computation {
                     empHr=0;
                     break;
             }
-            empWwage=empHr*Daily_wage_per_hour;
-            totalEmpWage+=empWwage;
-            System.out.println("wage of employee is "+empWwage);
+            totalEmpHrs+=empHr;
+
+            System.out.println("Day#: "+totalWorkingDays+"Emp Hr: "+empHr);
 
         }
+        int totalEmpWage=totalEmpHrs*Daily_wage_per_hour;
         System.out.println("total employee wage :" + totalEmpWage);
     }
 }
