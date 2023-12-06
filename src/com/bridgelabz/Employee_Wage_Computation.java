7eb99e56aa1ce8835f8be12d5184652c868e8b13
pa1zore <pavanzore345@gmail.com>
2023-12-06 21:31:52 +0530

    Added: Part time Employee and Wage

--- src/com/bridgelabz/Employee_Wage_Computation.java
+++ src/com/bridgelabz/Employee_Wage_Computation.java
@@ -3,18 +3,25 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class Employee_Wage_Computation {
-    final static int check=1;
+    final static int full_time=1;
+    final static int part_time=2;
+
     final static int Daily_wage_per_hour=20;
     public static void main(String[] args) {
         int emp_hr=0;
         int emp_wage=0;
         Random r= new Random();
-        int current=r.nextInt(2);
-        if(check==current)
+        int current=r.nextInt(3);
+        if(full_time==current)
         {
-            System.out.println("employee is present");
+            System.out.println("employee is present full day");
             emp_hr=8;
 
+        }
+        else if(part_time==current)
+        {
+            System.out.println("employee is present part time");
+            emp_hr=4;
         }
         else
         {
