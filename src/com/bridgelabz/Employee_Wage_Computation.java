167082eb6ef0cc5d1f2c87901f6cfaf4c4cb19c9
pa1zore <pavanzore345@gmail.com>
2023-12-06 21:23:42 +0530

    Fist commit

---
+++ src/com/bridgelabz/Employee_Wage_Computation.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class Employee_Wage_Computation {
+    public static void main(String[] args) {
+        System.out.println("Welcom to Employee Wage Computation");
+    }
+}
